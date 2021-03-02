40750a3f29b2930f73c31a098c9a4af100a43d75
jonatasrt <jonatasteixeira@hotmail.com.br>
2021-03-02 16:52:35 -0300

    1.0.0

---
+++ bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+use jonatasrt\phpsms;
+
+require __DIR__ . '/vendor/autoload.php';
+
+
+
+require_once('vendor/autoload.php');
+
+$a = new phpsms();
+
+ //$a->sms_get('51983291202', '', '');
+
+ 
+ var_dump($a->verifica_sms(519672)) ;
